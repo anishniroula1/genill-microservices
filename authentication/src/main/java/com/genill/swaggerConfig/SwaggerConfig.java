40c6fe85e41fd593ebc6b581237f8d94524632ac
anishniroula <anishniroula1@gmail.com>
2019-09-11 18:01:55 -0400

    Swagger Complete

--- authentication/src/main/java/com/genill/swaggerConfig/SwaggerConfig.java
+++ authentication/src/main/java/com/genill/swaggerConfig/SwaggerConfig.java
@@ -8,6 +8,13 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+
+
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Collections;
 
 @Configuration
@@ -15,8 +22,9 @@ import java.util.Collections;
 public class SwaggerConfig {
 
     @Bean
-    public Docket swaggerConfiguration() {
-
+    public Docket swaggerConfiguration() throws IOException, XmlPullParserException {
+        MavenXpp3Reader reader = new MavenXpp3Reader();
+        Model model = reader.read(new FileReader("pom.xml"));
         return new Docket(DocumentationType.SWAGGER_2).select()
                 .apis(RequestHandlerSelectors.basePackage("com.genill")).build()
                 .apiInfo(apiDetails());
--- zuulservice/src/main/java/com/genill/zuulservice/ZuulserviceApplication.java
+++ zuulservice/src/main/java/com/genill/zuulservice/ZuulserviceApplication.java
@@ -5,14 +5,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
+@EnableSwagger2
 public class ZuulserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulserviceApplication.class, args);
 	}
 
+	@Bean
+	UiConfiguration uiConfig() {
+		return new UiConfiguration("validatorUrl", "list", "alpha", "schema",
+				UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS, false, true, 60000L);
+
+	}
 }
